6aa21b67eaa1ca3058596c4cd19f6958637c2178
GoharAbbas <GoharAbbas2122804@gmail.com>
2025-08-16 21:23:12 +0500

    complete the animated hero section

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// @ts-check
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Add your config options here
+};
+
+module.exports = nextConfig;
---
+++ src/sections/Integrations.tsx
@@ -0,0 +1,77 @@
+import Tag from "@/components/Tag";
+import figmaIcon from "@/assets/images/figma-logo.svg";
+import framerLogo from "@/assets/images/framer-logo.svg";
+import notionLogo from "@/assets/images/notion-logo.svg";
+import slackLogo from "@/assets/images/slack-logo.svg";
+import relumeLogo from "@/assets/images/relume-logo.svg";
+import githubLogo from "@/assets/images/github-logo.svg";
+import Image from "next/image";
+import IntegrationCols from "@/components/IntegrationCols";
+
+const integrations = [
+    {
+        name: "Figma",
+        icon: figmaIcon,
+        description: "Figma is a collaborative interface design tool.",
+    },
+    {
+        name: "Notion",
+        icon: notionLogo,
+        description: "Notion is an all-in-one workspace for notes and docs.",
+    },
+    {
+        name: "Slack",
+        icon: slackLogo,
+        description: "Slack is a powerful team communication platform.",
+    },
+    {
+        name: "Relume",
+        icon: relumeLogo,
+        description: "Relume is a no-code website builder and design system.",
+    },
+    {
+        name: "Framer",
+        icon: framerLogo,
+        description: "Framer is a professional website prototyping tool.",
+    },
+    {
+        name: "GitHub",
+        icon: githubLogo,
+        description: "GitHub is the leading platform for code collaboration.",
+    },
+];
+
+export type IntegrationType = typeof integrations;
+
+
+export default function Integrations() {
+    return (
+        <section className="py-24 overflow-hidden">
+            <div className="container">
+
+
+                <div className="grid lg:grid-cols-2 items-center lg:gap-16">
+                    <div>
+                        <Tag>Integrations</Tag>
+                        <h2 className="text-6xl font-medium  mt-6">
+                            Plays well with{" "}
+                            <span className="text-lime-400">others</span>
+                        </h2>
+                        <p className="text-white/50 mt-4 text-lg ">
+                            Layers seamlessly connect with your favorite tools making it
+                            easy to plug into any workflow and collaborate across
+                            platforms.
+                        </p>
+                    </div>
+
+                    <div>
+                        <div className="h-[400px] lg:h-[800px] mt-8  lg:mt-0   grid md:grid-cols-2 gap-4  overflow-hidden [mask-image:linear-gradient(to_bottom,transparentblack_10%,black_90%,transparent)]">
+                            <IntegrationCols integrations={integrations} />
+                            <IntegrationCols integrations={integrations.slice().reverse()} className="hidden md:flex " />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
